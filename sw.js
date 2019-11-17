d05d84345396aa2401f1fd57e983022879b2f291
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-11-17 22:14:47 +0630

    Implementing the delete recipe for both  online and offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v3';
-const dynamicCacheName = 'site-dynamic-v4';
+const staticCacheName = 'site-static-v5';
+const dynamicCacheName = 'site-dynamic-v8';
 const assets = [
   '/',
   '/index.html',
